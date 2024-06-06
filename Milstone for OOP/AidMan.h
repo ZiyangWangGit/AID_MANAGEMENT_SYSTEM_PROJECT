d75a5984232db0bdc5eac37aa0af9fd53fab7a4e
ZiyangWangGit <148592435+ZiyangWangGit@users.noreply.github.com>
2024-06-05 20:14:27 -0400

    Update AidMan.h

--- Milstone for OOP/AidMan.h
+++ Milstone for OOP/AidMan.h
@@ -19,6 +19,38 @@
 #ifndef SDDS_AIDMAN_H_
 #define SDDS_AIDMAN_H_
 
+#include "Menu.h"
+#include "iProduct.h"
+
+const int noFileLength = 8;
+const int contentLength = 200;
+
+
+namespace sdds {
+	const int sdds_max_num_items = 100;
+
+	class AidMan {
+		char* fileName;
+		int productItems;
+		iProduct* productArray[sdds_max_num_items];
+		Menu mainMenu;
+		unsigned int menu();
+
+	public:
+		AidMan();
+		AidMan(const char* filename);
+		~AidMan();
+		void run();
+		void save();
+		bool load();
+		int search(int sku) const;
+		int list(const char* sub_desc = nullptr);
+		void remove(int index);
+		void sort();
+		void shipItem();
+	};
+
+}
 
 
 #endif
